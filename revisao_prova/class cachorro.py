0dd21f2f3d9b39dc821c7ca7b0dc0a2a408cf579
bianca-silva-30 <iphonedabianca30@gmail.com>
2025-12-09 20:06:15 -0300

    class cachorro

---
+++ revisao_prova/class cachorro.py
@@ -0,0 +1,33 @@
+class Cachorro:
+
+    def __init__(self, nome, raca, idade):
+        self.nome = nome
+        self.raca = raca
+        self. idade = idade
+
+    # metodo: comportamento/acao
+    def latir(self):
+        return f'{self.nome} o cachorro está latindo: AU AU'
+
+    #metodo que modifica o estado do objeto
+    def fazer_aniversario(self):
+        self.idade += 1 #modifica o atributo idade
+        return f'Parabéns! {self.nome} agora tem {self.idade} anos!'
+
+    #metodo: comportamento que usa multiplos atributos
+    def apresentar(self):
+        return f'Olá,eu sou {self.nome} um {self.raca} e tenho {self.idade} anos!'
+dog_a = Cachorro('Max', 'Golden Retriver', 5)
+dog_b = Cachorro('Luna', "Lulu da Pomerania", 6)
+
+print(dog_a.apresentar())
+print(dog_a.latir())
+print(dog_a.fazer_aniversario())
+
+print(dog_b.apresentar())
+print(dog_b.latir())
+print(dog_b.fazer_aniversario())
+
+#alterando o estado do max atraves de um metodo
+print(dog_a.fazer_aniversario())
+print(dog_a.apresentar()) #verifica a idade atualizada
